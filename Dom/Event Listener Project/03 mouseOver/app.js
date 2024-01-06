9fe8e492facba05d1d1a6d40bfa9c21331a20fb7
rajnish98 <rajnishpandey63@gmail.com>
2024-01-06 20:56:00 +0530

    mouseover

---
+++ Dom/Event Listener Project/03 mouseOver/app.js
@@ -0,0 +1,7 @@
+const test = document.getElementById("#box");
+const btn = document.querySelector("p");
+
+btn.addEventListener("mouseover", function(){
+  alert("you enter in mouseover area");
+});
+
