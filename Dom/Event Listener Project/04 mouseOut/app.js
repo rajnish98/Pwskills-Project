0b0c45630afba7d3cc90d3aa38fadf1c41143e08
rajnish98 <rajnishpandey63@gmail.com>
2024-01-06 21:54:13 +0530

    onekeypress

---
+++ Dom/Event Listener Project/04 mouseOut/app.js
@@ -0,0 +1,6 @@
+const box = document.getElementById("#box");
+const test = document.querySelector("p");
+
+test.addEventListener("mouseout", function(){
+  alert("you are enter in mouseout area");
+});
---
+++ Dom/Event Listener Project/05 onKeyPress/app.js
@@ -0,0 +1,9 @@
+const log1 = document.getElementById("log1");
+const log = document.getElementById("log");
+const input = document.querySelector("input");
+
+input.addEventListener("keypress", logKey);
+
+function logKey(e) {
+  log.textContent += ` ${e.code}`;
+}
