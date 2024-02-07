91e40f2b99fadaf510c5ea308d63f220b46dd3aa
rajnish98 <rajnishpandey63@gmail.com>
2024-02-07 21:12:04 +0530

    calculator app

---
+++ Milestone Assignment/02 Calculator/script.js
@@ -0,0 +1,31 @@
+
+function calculate() 
+{
+  let num1 = parseInt(document.getElementById("num1").value)
+  let num2 = parseInt(document.getElementById("num2").value);
+  let operator = document.getElementById("operator").value;
+
+  let result;
+  switch (operator)
+   {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+
+    default:
+      result = "Invalid Operator!";
+      break;
+
+  }
+
+  document.getElementById("result").value = result;
+}
