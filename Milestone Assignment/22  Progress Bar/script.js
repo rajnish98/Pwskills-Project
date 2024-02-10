a811774cbd0f45e233eb2cacc7ff9abd69268f40
rajnish98 <rajnishpandey63@gmail.com>
2024-02-10 17:43:50 +0530

    progress bar

--- Milestone Assignment/22  Progress Bar/script.js
+++ Milestone Assignment/22  Progress Bar/script.js
@@ -1,9 +1,9 @@
 const progressbar = document.querySelector(".progressbar");
 window.addEventListener("scroll", () => {
-  const scrollTop = window.scrollX; // calculte scroll top in px
+  const scrollTop = window.scrollY; // calculte scroll top in px
 
   const docHeight  = document.body.clientHeight - window.innerHeight; // calculate document height
   const scrollPercent = (scrollTop / docHeight) * 100;
 
-  progressbar.style.width = scrollPercent * "%";
+  progressbar.style.width = scrollPercent + "%";
 })
